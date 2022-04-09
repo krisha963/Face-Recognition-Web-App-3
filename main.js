1468494e58b2981144fb5f650d60c5c36d1d78d6
krisha963 <89096112+krisha963@users.noreply.github.com>
2022-04-09 13:34:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.attach('#camera');
+camera = document.getElementById("camera");
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+       document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'"/>';
+       Webcam.attach('#camera');
+       camera = document.getElementById("camera");
+   });
+}
+console.log('ml5 version',ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9sMKCA1bL/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('model Loaded!');
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
